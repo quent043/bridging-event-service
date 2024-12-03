e08d8ccc8b2e4cd6652c0a402c4e119ea4b94c14
Quentin D.C <quent043@hotmail.com>
2024-12-03 19:12:42 +0000

    Added Todo

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// TODO
+// Finish general setup
+// Prisma models (event data; data aggregator with percentages etc)
+// Install service event listener with simple logs
+// Create extra controller for event data OR live stream or websocket canal
+// Implement Redis ===> Where do we store data with redis ?
+// How much historical data do we store ?
+// Do we index past data first ?
+// Extra: Add custom error
+// Extra: Add Demo frontend
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
