db10c60878d3dd6249fdcfdaa45c3556427bec9a
Quentin D.C <quent043@hotmail.com>
2024-12-03 20:37:31 +0000

    Updated deps

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { BridgeEventListenerModule } from './bridge-event/bridge-event-listener.module';
 import { ViemModule } from './viem/viem.module';
+import { MetricsModule } from './metrics/metrics.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [PrismaModule, BridgeEventListenerModule, ViemModule],
+  imports: [PrismaModule, BridgeEventListenerModule, ViemModule, MetricsModule, RedisModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/bridge-event/bridge-event-listener.module.ts
+++ src/bridge-event/bridge-event-listener.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BridgeEventListenerService } from './bridge-event-listener.service';
 import {PrismaService} from "../prisma/prisma.service";
+import {RedisService} from "../redis/redis.service";
 
 @Module({
-  providers: [BridgeEventListenerService, PrismaService],
+  providers: [BridgeEventListenerService, PrismaService, RedisService],
 })
 export class BridgeEventListenerModule {}
