0610eb13cd4525663ed9751c9c00f60481ec1d83
Quentin D.C <quent043@hotmail.com>
2024-12-04 09:07:59 +0000

    Added deps

--- src/metrics/metrics.module.ts
+++ src/metrics/metrics.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MetricsService } from './metrics.service';
+import {RedisService} from "../redis/redis.service";
 
 @Module({
-  providers: [MetricsService]
+  providers: [MetricsService, RedisService]
 })
 export class MetricsModule {}
