6c2a503c9fda9eaf06cdf7ee8c02299761752731
Quentin D.C <quent043@hotmail.com>
2024-12-04 11:21:39 +0000

    WIP

--- src/metrics/metrics.service.ts
+++ src/metrics/metrics.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { RedisService } from '../redis/redis.service';
+import {SocketBridgeEventLog} from "../../types";
 
 @Injectable()
 export class MetricsService {
@@ -7,14 +8,27 @@ export class MetricsService {
 
     private redisClient = this.redisService.getClient();
 
-    //TODO: type event
-    async processBridgeEvent(eventData: any) {
-        const { token, amount, user } = eventData;
+    //TODO: type event & Batch promisse.all
+    async processBridgeEvent(eventData: SocketBridgeEventLog) {
+        console.log('Processing event:', eventData);
 
         // Aggregate metrics in Redis
         const timestamp = Date.now();
 
+        // Add the event's amount with a timestamp to a sorted set
+        await this.redisClient.zAdd('bridge_events:amounts', {
+            score: timestamp,
+            value: eventData.args.amount.toString(),
+        });
 
+        // Increment the volume of the token in a hash
+        const amountAsNumber = Number(eventData.args.amount);
+
+        if (!Number.isSafeInteger(amountAsNumber)) {
+            console.warn('Amount exceeds safe integer range, precision might be lost:', eventData.args.amount);
+        }
+
+        await this.redisClient.hIncrBy('bridge_events:top_tokens', eventData.args.token, amountAsNumber);
 
         console.log('Event processed and metrics updated.');
     }
