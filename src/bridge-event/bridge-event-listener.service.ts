b8f708d4a36c61da4ec100283e825e560066fb65
Quentin D.C <quent043@hotmail.com>
2024-12-04 09:08:41 +0000

    Updated bridge-event-listener

--- src/bridge-event/bridge-event-listener.service.ts
+++ src/bridge-event/bridge-event-listener.service.ts
@@ -1,23 +1,25 @@
 import { Injectable, Inject, OnModuleInit } from '@nestjs/common';
 import {createPublicClient, http, PublicClient} from 'viem';
-import { ethers } from 'ethers';
+import {ethers, Log} from 'ethers';
 // import redisClient from '../redis-client';
 import {mainnet} from "viem/chains";
 import {SocketGatewayABI} from "../../contracts/ABI/SocketGateway";
+import {RedisService} from "../redis/redis.service";
+import {MetricsService} from "../metrics/metrics.service";
 
 @Injectable()
 export class BridgeEventListenerService implements OnModuleInit {
-    // private publicClient: PublicClient;
-    private contractAddress = '0x3a23F943181408EAC424116Af7b7790c94Cb97a5';
-    private abi = SocketGatewayABI;
+    private readonly contractAddress = '0x3a23F943181408EAC424116Af7b7790c94Cb97a5';
+    private readonly abi = SocketGatewayABI;
 
-    constructor(@Inject('PUBLIC_CLIENT') private publicClient: PublicClient) {}
-
+    constructor(
+        @Inject('PUBLIC_CLIENT') private publicClient: PublicClient,
+        private readonly redisService: RedisService,
+        private readonly metricsService: MetricsService,
+    ) {}
 
     async onModuleInit() {
-
-        // Begin listening to events
-        console.log('listenToEvents...')
+        console.log('Initializing event listener...');
         this.listenToEvents();
     }
 
@@ -29,12 +31,13 @@ export class BridgeEventListenerService implements OnModuleInit {
             abi: this.abi,
             eventName,
             onLogs: async (logs) => {
-
                 for (const log of logs) {
                     console.log('Event received:', log);
 
-                    // Publish the event to Redis for processing
-                    // await redisClient.publish('bridge_events', JSON.stringify(log.args));
+                    // TODO: Check what data to push to the queue
+                    await this.metricsService.processBridgeEvent(log);
+                    //TODO chépa si ça sert ça
+                    await this.redisService.publish('bridge_events', JSON.stringify(log));
                 }
             },
         });
