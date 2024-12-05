db7c40205b3847f8167fd5904b934ad3ea61f291
Quentin D.C <quent043@hotmail.com>
2024-12-05 18:37:53 +0000

    Updated gateway events

--- src/metrics/metrics.gateway.ts
+++ src/metrics/metrics.gateway.ts
@@ -16,7 +16,6 @@ export class MetricsGateway
   @WebSocketServer()
   server!: Server;
 
-  private redisSubscriber: any;
   private logger: Logger = new Logger('MetricsGateway');
 
   constructor(private readonly redisService: RedisService) {}
@@ -24,10 +23,7 @@ export class MetricsGateway
   async afterInit() {
     this.logger.log('WebSocket Gateway Initialized');
 
-    this.redisSubscriber = this.redisService.getClient().duplicate();
-    await this.redisSubscriber.connect();
-
-    await this.redisSubscriber.subscribe(
+    await this.redisService.subscribe(
         'bridge_events:processed_updates',
         (message: any) => {
           const processedData = JSON.parse(message);
@@ -44,8 +40,17 @@ export class MetricsGateway
     this.logger.log(`Client disconnected: ${client.id}`);
   }
 
-  private broadcastUpdate(data: { token: string; totalVolume: string }) {
-    this.logger.log('Broadcasting update to clients:', data);
-    this.server.emit('bridge_event_update', data);
+  private broadcastUpdate(update: any) {
+    if (update.type === 'token_update') {
+      this.server.emit('token_volume_update', {
+        token: update.token,
+        totalVolume: update.totalVolume,
+      });
+    } else if (update.type === 'chain_update') {
+      this.server.emit('chain_volume_update', {
+        chainId: update.chainId,
+        totalVolume: update.totalVolume,
+      });
+    }
   }
 }
