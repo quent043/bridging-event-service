ec6a7bf25e11f46a743a3fada3a6bcd423916c45
Quentin D.C <quent043@hotmail.com>
2024-12-06 15:53:45 +0000

    FInalized listener

--- src/bridge-event/bridge-event-listener.service.ts
+++ src/bridge-event/bridge-event-listener.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { Log, PublicClient } from 'viem';
 import { SocketGatewayABI } from '../../contracts/ABI/SocketGateway';
 import { MetricsService } from '../metrics/metrics.service';
@@ -8,6 +8,8 @@ import { SocketBridgeEventLog } from '../../types';
 export class BridgeEventListenerService implements OnModuleInit {
   private readonly contractAddress = '0x3a23F943181408EAC424116Af7b7790c94Cb97a5';
   private readonly abi = SocketGatewayABI;
+
+  private logger: Logger = new Logger('MetricsGateway');
 
   constructor(
     @Inject('PUBLIC_CLIENT') private publicClient: PublicClient,
@@ -24,57 +26,29 @@ export class BridgeEventListenerService implements OnModuleInit {
 
     this.publicClient.watchContractEvent({
       address: this.contractAddress as `0x${string}`,
-      //TODO: need other events in ABI  ?
       abi: this.abi,
       eventName,
       onLogs: async (logs: Log[]) => {
-        for (const log of logs as unknown as SocketBridgeEventLog[]) {
-          console.log('Number of events received:', logs.length);
-          console.log(
-            'Raw Event Log Received:',
-            log.eventName.toString(),
-            log.blockNumber?.toString(),
-            log.args.amount.toString(),
-            log.args.toChainId.toString(),
+        this.logger.log('Number of events received:', logs.length);
+
+        try {
+          await Promise.all(
+            logs.map(async (log: Log) => {
+              const socketLog = log as unknown as SocketBridgeEventLog;
+              console.log(
+                'Raw Event Log Received:',
+                socketLog.eventName.toString(),
+                socketLog.blockNumber?.toString(),
+                socketLog.args.amount.toString(),
+                socketLog.args.toChainId.toString(),
+              );
+              await this.metricsService.processBridgeEvent(socketLog);
+            }),
           );
-          try {
-            await this.metricsService.processBridgeEvent(log);
-          } catch (error) {
-            console.error('Failed to process event log:', error);
-          }
+        } catch (error) {
+          this.logger.error('Failed to process one or more event logs:', error);
         }
       },
     });
   }
 }
-
-// private async listenToEvents() {
-//   const eventName = 'SocketBridge';
-//
-//   this.publicClient.watchContractEvent({
-//     address: this.contractAddress as `0x${string}`,
-//     //TODO: need other events in ABI  ?
-//     abi: this.abi,
-//     eventName,
-//     onLogs: async (logs: Log[]) => {
-//       for (const log of logs as unknown as SocketBridgeEventLog[]) {
-//         console.log('Number of events received:', logs.length);
-//         console.log(
-//             'Raw Event Log Received:',
-//             log.eventName.toString(),
-//             log.blockNumber?.toString(),
-//             log.args.amount.toString(),
-//             log.args.toChainId.toString(),
-//         );
-//         try {
-//           await this.metricsService.processBridgeEvent(log);
-//         } catch (error) {
-//           console.error('Failed to process event log:', error);
-//         }
-//       }
-//     },
-//   });
-// }
-// }
-//
-// optimise here with promise all in the loop
