6034881379dc8898b1705a7fbc2cc1f2a2694744
Quentin D.C <quent043@hotmail.com>
2024-12-08 11:15:31 +0000

    Added url to CORS

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
+    origin: [process.env.FRONTEND_URL || '', 'http://localhost:3001'],
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
--- src/metrics/metrics.gateway.ts
+++ src/metrics/metrics.gateway.ts
@@ -11,7 +11,7 @@ import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({
   cors: {
-    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
+    origin: [process.env.FRONTEND_URL, 'http://localhost:3001'],
     methods: ['GET', 'POST'],
     credentials: true,
   },
